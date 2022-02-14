093ac98a5882cb448c36ac8137f6d723335bd011
SergeyPoletaev <info.elekit@yandex.ru>
2022-02-14 16:26:25 +0300

    5. Очередь на двух стеках [#160]. Доработан метод poll.

--- src/main/java/ru/job4j/collections/linked/SimpleQueue.java
+++ src/main/java/ru/job4j/collections/linked/SimpleQueue.java
@@ -1,5 +1,7 @@
 package ru.job4j.collections.linked;
 
+import java.util.NoSuchElementException;
+
 public class SimpleQueue<T> {
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
@@ -12,6 +14,9 @@ public class SimpleQueue<T> {
     }
 
     public T poll() {
+        if (sizeIn == 0 && sizeOut == 0) {
+            throw new NoSuchElementException();
+        }
         if (sizeOut == 0) {
             while (sizeIn > 0) {
                 out.push(in.pop());
