964e20f1b92dc3f0a058edf1d266693ab2ecfc68
SergeyPoletaev <info.elekit@yandex.ru>
2022-02-15 09:58:16 +0300

    6. Перевернуть связанный список [#161]. Исправлена опечатка

--- src/main/java/ru/job4j/collections/linked/ForwardLinked.java
+++ src/main/java/ru/job4j/collections/linked/ForwardLinked.java
@@ -45,8 +45,8 @@ public class ForwardLinked<T> implements Iterable<T> {
                 curr.next = head;
                 head = curr;
                 curr = next;
-                rsl = true;
             }
+            rsl = true;
         }
         return rsl;
     }
