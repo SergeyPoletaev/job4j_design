d49266e1313c18db22f77b5df13ab2f81d1bf55b
SergeyPoletaev <info.elekit@yandex.ru>
2022-02-17 13:04:10 +0300

    5. Переопределить и equals и hashCode [#1002 #265799]

--- src/main/java/ru/job4j/collections/map/UserProgram.java
+++ src/main/java/ru/job4j/collections/map/UserProgram.java
@@ -10,12 +10,9 @@ public class UserProgram {
     public static void main(String[] args) {
         User userFirst = new User("Anna", 2, new GregorianCalendar(2000, Calendar.APRIL, 12));
         User userSecond = new User("Anna", 2, new GregorianCalendar(2000, Calendar.APRIL, 12));
-        Map<User, Object> map = new HashMap<>(
-                Map.of(
-                        userFirst, new Object(),
-                        userSecond, new Object()
-                )
-        );
+        Map<User, Object> map = new HashMap<>();
+        map.put(userFirst, new Object());
+        map.put(userSecond, new Object());
         System.out.println(map);
     }
 }
