d49a8b14ea7bc4c6d2698406771ba40b677f8164
SergeyPoletaev <info.elekit@yandex.ru>
2022-02-24 22:17:36 +0300

    0.1. FileOutputStream. [#252488 #268639]. Оптимизация метода.

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -10,12 +10,10 @@ public class ResultFile {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             for (int i = 1; i <= 10; i++) {
                 for (int j = 1; j <= 10; j++) {
-                    String str = i + " * " + j + " = " + i * j + System.lineSeparator();
+                    String str = i + " * " + j + " = " + i * j + "   ";
                     out.write(str.getBytes(StandardCharsets.UTF_8));
-                    if (j == 10) {
-                        out.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
-                    }
                 }
+                out.write(System.lineSeparator().getBytes(StandardCharsets.UTF_8));
             }
         } catch (IOException e) {
             e.printStackTrace();
