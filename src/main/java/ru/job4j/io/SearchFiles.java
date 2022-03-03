10a105748c4e5d00ecc4b48d3a82ac8d80f73785
SergeyPoletaev <info.elekit@yandex.ru>
2022-03-03 18:00:06 +0300

    Добавлены модификаторы доступа к полям класса.

--- src/main/java/ru/job4j/io/SearchFiles.java
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -12,8 +12,8 @@ import java.util.function.Predicate;
 import static java.nio.file.FileVisitResult.CONTINUE;
 
 public class SearchFiles implements FileVisitor<Path> {
-    Predicate<Path> condition;
-    List<Path> paths = new ArrayList<>();
+    private final Predicate<Path> condition;
+    private final List<Path> paths = new ArrayList<>();
 
     public SearchFiles(Predicate<Path> condition) {
         this.condition = condition;
