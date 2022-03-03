d54f234f0a7e18cf09d1cfa42ec0951fb161dfb1
SergeyPoletaev <info.elekit@yandex.ru>
2022-03-03 18:00:28 +0300

    5.2. Архивировать проект [#861]

---
+++ src/main/java/ru/job4j/io/Zip.java
@@ -0,0 +1,43 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Zip {
+
+    public static void main(String[] args) {
+        Zip zip = new Zip();
+        zip.packSingleFile(
+                new File("./pom.xml"),
+                new File("./pom.zip")
+        );
+    }
+
+    public void packFiles(List<Path> source, Path target) {
+        try (ZipOutputStream zip =
+                     new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target.toFile())))) {
+            for (Path path : source) {
+                zip.putNextEntry(new ZipEntry(path.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toFile()))) {
+                    zip.write(out.readAllBytes());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void packSingleFile(File source, File target) {
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            zip.putNextEntry(new ZipEntry(source.getPath()));
+            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+                zip.write(out.readAllBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/io/ZipProgram.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ZipProgram {
+
+    public static void main(String[] args) throws IOException {
+        if (!(args.length == 3)) {
+            String ln = System.lineSeparator();
+            throw new IllegalArgumentException(
+                    "Invalid number of arguments. Use the following arguments to start: " + ln
+                            + "-d=source_directory" + ln
+                            + "-e=exclude_files" + ln
+                            + "-o=output_file" + ln
+            );
+        }
+        ArgsName argsName = ArgsName.of(args);
+        Path dir = Path.of(argsName.get("d"));
+        if (!dir.toFile().exists() || !dir.toFile().isDirectory()) {
+            throw new IllegalArgumentException("Archived directory does not exist");
+        }
+        Path target = Path.of(argsName.get("o"));
+        List<Path> source = search(dir, p -> !p.toFile().getName().endsWith(argsName.get("e")));
+        new Zip().packFiles(source, target);
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+}
