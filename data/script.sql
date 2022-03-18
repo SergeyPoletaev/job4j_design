53401ad05344ea87ea818d0d354d7df8ac1afdab
SergeyPoletaev <info.elekit@yandex.ru>
2022-03-19 00:41:02 +0300

    0. Что такое база данных. [#6860]

---
+++ data/script.sql
@@ -0,0 +1,15 @@
+create table users
+(
+    id      serial primary key,
+    name    varchar(255),
+    age     integer,
+    status  boolean,
+    address text
+);
+insert into users(name, age, status, address)
+values ('anna', 12, true, '141320, Peresvet');
+update users
+set age    = 20,
+    status = false;
+delete
+from users;
