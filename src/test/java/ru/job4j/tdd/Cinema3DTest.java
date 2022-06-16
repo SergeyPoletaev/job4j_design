3912358551f8db02a291ace4da39fe323fd50f69
spoletaev <info.elekit@yandex.ru>
2022-06-16 08:28:06 +0300

    2. Что такое TDD? [#4918]. Добавлены тесты.

--- src/test/java/ru/job4j/tdd/Cinema3DTest.java
+++ src/test/java/ru/job4j/tdd/Cinema3DTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.tdd;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Calendar;
@@ -12,6 +13,7 @@ import static org.hamcrest.Matchers.is;
 
 public class Cinema3DTest {
 
+    @Ignore
     @Test
     public void whenFind() {
         Cinema cinema = new Cinema3D();
@@ -24,6 +26,7 @@ public class Cinema3DTest {
         assertThat(sessions, is(List.of(sessionFirst, sessionSecond)));
     }
 
+    @Ignore
     @Test
     public void whenNotFound() {
         Cinema cinema = new Cinema3D();
@@ -31,6 +34,7 @@ public class Cinema3DTest {
         assertThat(sessions, is(empty()));
     }
 
+    @Ignore
     @Test
     public void whenBuy() {
         Account account = new AccountCinema();
@@ -39,5 +43,36 @@ public class Cinema3DTest {
         date.set(2022, Calendar.JUNE, 15, 15, 0);
         Ticket ticket = cinema.buy(account, 1, 1, date);
         assertThat(ticket, is(new Ticket3D()));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyTicketWithInvalidSeat() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, Calendar.JUNE, 15, 15, 0);
+        cinema.buy(account, -1, 1, date);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyTicketWithInvalidDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(1977, Calendar.JUNE, 15, 15, 0);
+        cinema.buy(account, 1, 1, date);
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void whenBuyTicketWithOccupiedSeat() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, Calendar.JUNE, 15, 15, 0);
+        cinema.buy(account, 1, 1, date);
+        cinema.buy(account, 1, 1, date);
     }
 }
